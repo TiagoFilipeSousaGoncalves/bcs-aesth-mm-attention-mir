3402fc613e4db87dd96801ae57cfc7f9d48742e9
TiagoFilipeSousaGoncalves <tiago.galves@hotmail.com>
2024-11-03 16:07:31 +0000

    Updated: scripts/train_imgmodels.sh

--- scripts/train_imgmodels.sh
+++ scripts/train_imgmodels.sh
@@ -12,7 +12,7 @@ python src/main_image.py \
  --gpu_id 0 \
  --config_json 'config/config_image.json' \
  --images_resized_path '/nas-ctm01/datasets/private/CINDERELLA/experiments/retrieval/tgoncalv/breloai-rsz' \
- --images_original_path '/nas-ctm01/datasets/private/CINDERELLA/breloai-web-db' \ 
+ --images_original_path '/nas-ctm01/datasets/private/CINDERELLA/breloai-web-db' \
  --csvs_path '/nas-ctm01/datasets/private/CINDERELLA/breloai-web-db/csvs' \
  --pickles_path '/nas-ctm01/datasets/private/CINDERELLA/experiments/retrieval/tgoncalv/pickles' \
  --results_path '/nas-ctm01/datasets/private/CINDERELLA/experiments/retrieval/tgoncalv/results' \
