7bb96d28995571ecd0f211a6045977d5769a3fb9
TiagoFilipeSousaGoncalves <tiago.galves@hotmail.com>
2024-11-04 09:46:23 +0000

    Updated: scripts/image

--- scripts/image/DeiT_Base_Patch16_224.sh
+++ scripts/image/DeiT_Base_Patch16_224.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-#SBATCH --partition=gpu_min11gb
-#SBATCH --qos=gpu_min11gb
+#SBATCH --partition=gpu_min12gb
+#SBATCH --qos=gpu_min12gb
 #SBATCH --job-name=cind_breloai_att_ret
 #SBATCH --output=DeiT_Base_Patch16_224.out
 #SBATCH --error=DeiT_Base_Patch16_224.err
