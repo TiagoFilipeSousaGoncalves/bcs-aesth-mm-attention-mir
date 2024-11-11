1a7e65e5b20ced9b4e5a421e821c6dea96f65bbe
TiagoFilipeSousaGoncalves <tiago.galves@hotmail.com>
2024-11-11 10:16:04 +0000

    Updated: scripts/image/

--- scripts/image/MaxViT_Tiny_224.sh
+++ scripts/image/MaxViT_Tiny_224.sh
@@ -8,22 +8,24 @@
 
 
 echo "CINDERELLA BreLoAI Retrieval: A Study with Attention Mechanisms"
-# python src/main_image.py \
-#  --gpu_id 0 \
-#  --config_json 'config/image/E/MaxViT_Tiny_224.json' \
-#  --images_resized_path '/nas-ctm01/datasets/private/CINDERELLA/experiments/retrieval/tgoncalv/breloai-rsz' \
-#  --images_original_path '/nas-ctm01/datasets/private/CINDERELLA/breloai-web-db' \
-#  --csvs_path '/nas-ctm01/datasets/private/CINDERELLA/breloai-web-db/csvs' \
-#  --pickles_path '/nas-ctm01/datasets/private/CINDERELLA/experiments/retrieval/tgoncalv/pickles' \
-#  --results_path '/nas-ctm01/datasets/private/CINDERELLA/experiments/retrieval/tgoncalv/results'
-# echo "Finished"
+echo "Catalogue Type: E"
+python src/main_image.py \
+ --gpu_id 0 \
+ --config_json 'config/image/E/MaxViT_Tiny_224.json' \
+ --images_resized_path '/nas-ctm01/datasets/private/CINDERELLA/experiments/retrieval/tgoncalv/breloai-rsz/E' \
+ --images_original_path '/nas-ctm01/datasets/private/CINDERELLA/breloai-web-db' \
+ --csvs_path '/nas-ctm01/datasets/private/CINDERELLA/breloai-web-db/csvs' \
+ --pickles_path '/nas-ctm01/datasets/private/CINDERELLA/experiments/retrieval/tgoncalv/pickles/E' \
+ --results_path '/nas-ctm01/datasets/private/CINDERELLA/experiments/retrieval/tgoncalv/results/E'
+echo "Finished"
 
+echo "Catalogue Type: F"
 python src/main_image.py \
  --gpu_id 0 \
  --config_json 'config/image/F/MaxViT_Tiny_224.json' \
- --images_resized_path '/nas-ctm01/datasets/private/CINDERELLA/experiments/retrieval/tgoncalv/breloai-rsz' \
+ --images_resized_path '/nas-ctm01/datasets/private/CINDERELLA/experiments/retrieval/tgoncalv/breloai-rsz/F' \
  --images_original_path '/nas-ctm01/datasets/private/CINDERELLA/breloai-web-db' \
  --csvs_path '/nas-ctm01/datasets/private/CINDERELLA/breloai-web-db/csvs' \
- --pickles_path '/nas-ctm01/datasets/private/CINDERELLA/experiments/retrieval/tgoncalv/pickles' \
- --results_path '/nas-ctm01/datasets/private/CINDERELLA/experiments/retrieval/tgoncalv/results'
+ --pickles_path '/nas-ctm01/datasets/private/CINDERELLA/experiments/retrieval/tgoncalv/pickles/F' \
+ --results_path '/nas-ctm01/datasets/private/CINDERELLA/experiments/retrieval/tgoncalv/results/F'
 echo "Finished"
