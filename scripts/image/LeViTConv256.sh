5d2d40a2147605e1babe74a4285be37c1ba3d883
TiagoFilipeSousaGoncalves <tiago.galves@hotmail.com>
2024-11-15 09:08:36 +0000

    Updated scripts/image

--- scripts/image/LeViTConv256.sh
+++ scripts/image/LeViTConv256.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-#SBATCH --partition=gpu_min24gb
-#SBATCH --qos=gpu_min24gb
+#SBATCH --partition=gpu_min32gb
+#SBATCH --qos=gpu_min32gb
 #SBATCH --job-name=cind_breloai_att_ret
 #SBATCH --output=LeViTConv256.out
 #SBATCH --error=LeViTConv256.err
