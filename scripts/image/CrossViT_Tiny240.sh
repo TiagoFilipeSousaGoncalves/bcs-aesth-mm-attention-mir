53e41b376b053169e37f3da42ac003e782aaaa29
TiagoFilipeSousaGoncalves <tiago.galves@hotmail.com>
2024-11-16 22:19:07 +0000

    Updated: scripts/image/CrossViT_Tiny240.sh

--- scripts/image/CrossViT_Tiny240.sh
+++ scripts/image/CrossViT_Tiny240.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-#SBATCH --partition=gpu_min32gb
-#SBATCH --qos=gpu_min32gb
+#SBATCH --partition=gpu_min24gb
+#SBATCH --qos=gpu_min24gb
 #SBATCH --job-name=cind_breloai_att_ret
 #SBATCH --output=CrossViT_Tiny240.out
 #SBATCH --error=CrossViT_Tiny240.err
