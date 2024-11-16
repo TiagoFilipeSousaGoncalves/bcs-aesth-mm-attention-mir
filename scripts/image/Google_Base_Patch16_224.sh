d5468c489d44870ceb56a6f5954c972efaa05c44
TiagoFilipeSousaGoncalves <tiago.galves@hotmail.com>
2024-11-16 22:27:57 +0000

    Updated: scripts/image/Google_Base_Patch16_224.sh

--- scripts/image/Google_Base_Patch16_224.sh
+++ scripts/image/Google_Base_Patch16_224.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-#SBATCH --partition=gpu_min32gb
-#SBATCH --qos=gpu_min32gb
+#SBATCH --partition=gpu_min80gb
+#SBATCH --qos=gpu_min80gb
 #SBATCH --job-name=cind_breloai_att_ret
 #SBATCH --output=Google_Base_Patch16_224.out
 #SBATCH --error=Google_Base_Patch16_224.err
