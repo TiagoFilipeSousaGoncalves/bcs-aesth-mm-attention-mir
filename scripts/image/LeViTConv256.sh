29bcb35b579769dca868dce1870ff0f2a6c40ef6
TiagoFilipeSousaGoncalves <tiago.galves@hotmail.com>
2024-11-21 20:09:13 +0000

    Updated: scripts/image/LeViTConv256.sh

--- scripts/image/LeViTConv256.sh
+++ scripts/image/LeViTConv256.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-#SBATCH --partition=gpu_min32gb
-#SBATCH --qos=gpu_min32gb
+#SBATCH --partition=gpu_min24gb
+#SBATCH --qos=gpu_min24gb
 #SBATCH --job-name=cind_breloai_att_ret
 #SBATCH --output=LeViTConv256.out
 #SBATCH --error=LeViTConv256.err
@@ -8,26 +8,36 @@
 
 
 echo "CINDERELLA BreLoAI Retrieval: A Study with Attention Mechanisms"
-echo "Catalogue Type: E"
+# echo "Training Catalogue Type: E"
+# python src/main_image.py \
+#  --gpu_id 0 \
+#  --config_json 'config/image/E/LeViTConv256.json' \
+#  --pickles_path '/nas-ctm01/datasets/private/CINDERELLA/experiments/retrieval/tgoncalv/pickles/E' \
+#  --results_path '/nas-ctm01/datasets/private/CINDERELLA/experiments/retrieval/tgoncalv/results/E' \
+#  --train_or_test 'train'
+# echo "Finished"
+echo "Testing Catalogue Type: E"
 python src/main_image.py \
  --gpu_id 0 \
- --config_json 'config/image/E/LeViTConv256.json' \
- --images_resized_path '/nas-ctm01/datasets/private/CINDERELLA/experiments/retrieval/tgoncalv/breloai-rsz/E' \
- --images_original_path '/nas-ctm01/datasets/private/CINDERELLA/breloai-web-db' \
- --csvs_path '/nas-ctm01/datasets/private/CINDERELLA/breloai-web-db/csvs' \
  --pickles_path '/nas-ctm01/datasets/private/CINDERELLA/experiments/retrieval/tgoncalv/pickles/E' \
- --results_path '/nas-ctm01/datasets/private/CINDERELLA/experiments/retrieval/tgoncalv/results/E' \
- --train_or_test 'train'
+ --train_or_test 'test' \
+ --checkpoint_path '/nas-ctm01/datasets/private/CINDERELLA/experiments/retrieval/tgoncalv/results/E/2024-11-17_12-50-01/' \
+ --verbose
 echo "Finished"
 
-echo "Catalogue Type: F"
+# echo "Training Catalogue Type: F"
+# python src/main_image.py \
+#  --gpu_id 0 \
+#  --config_json 'config/image/F/LeViTConv256.json' \
+#  --pickles_path '/nas-ctm01/datasets/private/CINDERELLA/experiments/retrieval/tgoncalv/pickles/F' \
+#  --results_path '/nas-ctm01/datasets/private/CINDERELLA/experiments/retrieval/tgoncalv/results/F' \
+#  --train_or_test 'train'
+# echo "Finished"
+echo "Testing Catalogue Type: F"
 python src/main_image.py \
  --gpu_id 0 \
- --config_json 'config/image/F/LeViTConv256.json' \
- --images_resized_path '/nas-ctm01/datasets/private/CINDERELLA/experiments/retrieval/tgoncalv/breloai-rsz/F' \
- --images_original_path '/nas-ctm01/datasets/private/CINDERELLA/breloai-web-db' \
- --csvs_path '/nas-ctm01/datasets/private/CINDERELLA/breloai-web-db/csvs' \
  --pickles_path '/nas-ctm01/datasets/private/CINDERELLA/experiments/retrieval/tgoncalv/pickles/F' \
- --results_path '/nas-ctm01/datasets/private/CINDERELLA/experiments/retrieval/tgoncalv/results/F' \
- --train_or_test 'train'
+ --train_or_test 'test' \
+ --checkpoint_path '/nas-ctm01/datasets/private/CINDERELLA/experiments/retrieval/tgoncalv/results/F/2024-11-17_15-53-41/' \
+ --verbose
 echo "Finished"
