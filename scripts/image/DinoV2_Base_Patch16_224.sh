73ca9e0b47c436e6ab4291bfb0816015a5e379ad
TiagoFilipeSousaGoncalves <tiago.galves@hotmail.com>
2024-11-21 15:02:40 +0000

    Updated: scripts/image/DinoV2_Base_Patch16_224.sh

--- scripts/image/DinoV2_Base_Patch16_224.sh
+++ scripts/image/DinoV2_Base_Patch16_224.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-#SBATCH --partition=gpu_min80gb
-#SBATCH --qos=gpu_min80gb
+#SBATCH --partition=gpu_min12gb
+#SBATCH --qos=gpu_min12gb
 #SBATCH --job-name=cind_breloai_att_ret
 #SBATCH --output=DinoV2_Base_Patch16_224.out
 #SBATCH --error=DinoV2_Base_Patch16_224.err
@@ -8,26 +8,36 @@
 
 
 echo "CINDERELLA BreLoAI Retrieval: A Study with Attention Mechanisms"
-echo "Catalogue Type: E"
+# echo "Training Catalogue Type: E"
+# python src/main_image.py \
+#  --gpu_id 0 \
+#  --config_json 'config/image/E/DinoV2_Base_Patch16_224.json' \
+#  --pickles_path '/nas-ctm01/datasets/private/CINDERELLA/experiments/retrieval/tgoncalv/pickles/E' \
+#  --results_path '/nas-ctm01/datasets/private/CINDERELLA/experiments/retrieval/tgoncalv/results/E' \
+#  --train_or_test 'train'
+# echo "Finished"
+echo "Testing Catalogue Type: E"
 python src/main_image.py \
  --gpu_id 0 \
- --config_json 'config/image/E/DinoV2_Base_Patch16_224.json' \
- --images_resized_path '/nas-ctm01/datasets/private/CINDERELLA/experiments/retrieval/tgoncalv/breloai-rsz/E' \
- --images_original_path '/nas-ctm01/datasets/private/CINDERELLA/breloai-web-db' \
- --csvs_path '/nas-ctm01/datasets/private/CINDERELLA/breloai-web-db/csvs' \
  --pickles_path '/nas-ctm01/datasets/private/CINDERELLA/experiments/retrieval/tgoncalv/pickles/E' \
- --results_path '/nas-ctm01/datasets/private/CINDERELLA/experiments/retrieval/tgoncalv/results/E' \
- --train_or_test 'train'
+ --train_or_test 'test' \
+ --checkpoint_path '/nas-ctm01/datasets/private/CINDERELLA/experiments/retrieval/tgoncalv/results/E/2024-11-16_22-27-20/' \
+ --verbose
 echo "Finished"
 
-echo "Catalogue Type: F"
+# echo "Training Catalogue Type: F"
+# python src/main_image.py \
+#  --gpu_id 0 \
+#  --config_json 'config/image/F/DinoV2_Base_Patch16_224.json' \
+#  --pickles_path '/nas-ctm01/datasets/private/CINDERELLA/experiments/retrieval/tgoncalv/pickles/F' \
+#  --results_path '/nas-ctm01/datasets/private/CINDERELLA/experiments/retrieval/tgoncalv/results/F' \
+#  --train_or_test 'train'
+# echo "Finished"
+echo "Testing Catalogue Type: F"
 python src/main_image.py \
  --gpu_id 0 \
- --config_json 'config/image/F/DinoV2_Base_Patch16_224.json' \
- --images_resized_path '/nas-ctm01/datasets/private/CINDERELLA/experiments/retrieval/tgoncalv/breloai-rsz/F' \
- --images_original_path '/nas-ctm01/datasets/private/CINDERELLA/breloai-web-db' \
- --csvs_path '/nas-ctm01/datasets/private/CINDERELLA/breloai-web-db/csvs' \
  --pickles_path '/nas-ctm01/datasets/private/CINDERELLA/experiments/retrieval/tgoncalv/pickles/F' \
- --results_path '/nas-ctm01/datasets/private/CINDERELLA/experiments/retrieval/tgoncalv/results/F' \
- --train_or_test 'train'
+ --train_or_test 'test' \
+ --checkpoint_path '/nas-ctm01/datasets/private/CINDERELLA/experiments/retrieval/tgoncalv/results/F/2024-11-17_04-13-22/' \
+ --verbose
 echo "Finished"
